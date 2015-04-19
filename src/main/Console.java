603dee1b80e97a2f723456418ae9703f9590a5f4
Asymons <frostdragonz@live.com>
2015-04-18 22:25:24 -0400

    hey
    heyya

--- src/main/Console.java
+++ src/main/Console.java
@@ -15,6 +15,7 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
+import org.opencv.core.Point3;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -36,6 +37,24 @@ class DetectFaceDemo {
 	double width;
 	double height;
 	double ratio;
+	double focal;
+	focal = (169*17.49)/(54/12);
+	double distance = 0;
+	double distancewidth = 0;
+	double realwidth = 54.0;
+	double Bracket_Distance = 462;//462
+	double Sensor_Distance = 1.5;
+	double Target_Height = 110.125;
+	double Sensor_Height = 0.6125;
+	double Bracket_Height = 31.6875;
+	
+	double DeltaHeight = Target_Height - (Bracket_Height + Sensor_Height);
+	double Kinect_Distance = Bracket_Distance - Sensor_Distance;
+	double Yrot;
+	double Kinect_Angle = (Math.atan2(DeltaHeight,Kinect_Distance)*180/Math.PI);
+	
+	Point3 thing;
+	
     System.out.println("\nRunning DetectFaceDemo");
     Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,new Size(n,n));
     List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
@@ -67,17 +86,31 @@ class DetectFaceDemo {
         // Get bounding rect of contour
         
         Rect rect = Imgproc.boundingRect(points);
-        System.out.println(rect);
         width = rect.width;
         height = rect.height;
         ratio = width/height;
         
         //if(Math.abs(Imgproc.contourArea(contours.get(i))) > 3500 && Math.abs(Imgproc.contourArea(contours.get(i))) < 7000 && Imgproc.isContourConvex(points)){
         color = (color+1)*4;
-        if(Math.abs(ratio) > 2.5 && Math.abs(ratio) < 5 ){
+        if(Math.abs(ratio) > 2.5 && Math.abs(ratio) < 4.5){
+        	
+        System.out.println(rect);
+        System.out.println("Pixel Width: " + width);
         Imgproc.drawContours(imagegray,contours,i,new Scalar(0,255,0));
+        
          // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
         Core.rectangle(imagegray,rect.tl(), rect.br(), new Scalar(255, 100, 0),10, 8,0);
+        
+        //System.out.println("Focal Length: " + focal);
+        //distance = ((54/12)*focal)/width;
+        //distancewidth = (((52/12)*488/(rect.width)))/2;
+        //distance = (distancewidth)/(0.4142135624);
+        
+        thing = new Point3(rect.x - rect.width/2, (rect.y - rect.height/2),0);
+        Yrot = thing.y/(488/2)*22.5;
+        distance = (DeltaHeight / (Math.tan((Kinect_Angle + Yrot)*Math.PI/180)))/12;
+        
+        System.out.println("Distance: " + distance);
         
         	if(Math.abs(ratio) > 2.5 && Math.abs(ratio) < 5  ){
         		
@@ -85,7 +118,12 @@ class DetectFaceDemo {
         	}
        }
     }
+    
+    String textDistance = "Distance: " + String.valueOf(distance);
     Core.addWeighted(imagergb,1,imagegray,2,0, imagergb2);
+    Core.line(imagergb2, new Point(640/2, 0), new Point(640/2, 488), new Scalar(0,255,255), 1);
+    Core.line(imagergb2, new Point(0, 488/2), new Point(640, 488/2), new Scalar(0,255,255), 1);
+    Core.putText(imagergb2,textDistance, new Point(10,10),Core.FONT_HERSHEY_SIMPLEX,0.25, new Scalar(0,255,255));
     //Highgui.imwrite(path3, imagegray);
     new LoadImage(path3,imagergb2);
   }
